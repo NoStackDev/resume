9d9dc2bd15d18dfc7a3a25d4ec67a8a79b5d3b68
NoStackDev <partialstackdev@gmail.com>
2021-08-22 01:54:14 +0100

    nodemailer

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,3 +1,36 @@
+const nodemailer = require('nodemailer')
+
+require('dotenv').config()
+
 exports.formSubmit = (req, res) => {
-   res.render('success') 
+    const { name, email } = req.body
+    sendEmail(name, email)
+    res.render('success') 
+}
+
+
+// mail 
+const sendEmail = async (name, email) => {
+    // create SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAILPASSWORD
+        }
+    })
+
+    // message
+    const message = {
+        from: "Zino's Resume",
+        to: email,
+        subject: "no-reply",
+        html: `Howdy ${name}, this is a mail confirming that your message was received.
+               Please do not reply this message. I'll contact you in the next 12 hours.`
+    }
+
+    // send mail
+    try {
+        await transporter.sendMail(message)
+    } catch(err) { console.log(error) }
 }
